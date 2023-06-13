4df4078322a042f4608b33e056c3efedd7f16761
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-06-13 13:41:46 +0530

    reformated dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 import { NextPage } from "next";
 import Link from "next/link";
-import styles from "../../styles/App.module.css";
-import { getCurrentUser } from "@/hooks/getCurrentUser";
+import { getCurrentUser } from "@/actions/getCurrentUser";
 
 interface DashboardProps {
   appID: string;
@@ -11,41 +10,38 @@ const Dashboard: NextPage<DashboardProps> = async ({ appID }) => {
   const { props } = await getCurrentUser();
   // console.log(props);
 
-  const authorizedBody = (
-    <>
-      <p>You successfully signed in with Passage.</p>
-
-      <p>
-        Your username is: <b>{props.username}</b>
-      </p>
-
-      <p>
-        <passage-profile app-id={appID}></passage-profile>
-      </p>
-    </>
-  );
-
-  const unauthorizedBody = (
-    <>
-      You have not logged in and cannot view the dashboard.
-      <br /> <br />
-      <Link href="/" className={styles.link}>
-        Login to continue.
-      </Link>
-    </>
-  );
-
   return (
-    <div className={styles.mainContainer}>
-      <div className={styles.dashboard}>
-        <div className={styles.title}>
-          {props.isAuthorized ? "Welcome!" : "Unauthorized"}
-        </div>
-        <div className={styles.message}>
-          {props.isAuthorized ? authorizedBody : unauthorizedBody}
+    <main className="flex justify-center p-24 ">
+      <div className="border flex justify-center border-black rounded-xl w-96">
+        <div className="p-10 pb-5">
+          <div className="font-bold text-2xl mb-5">
+            {props.isAuthorized ? "Welcome!" : "Unauthorized"}
+          </div>
+          <div className="break-normal">
+            {props.isAuthorized ? (
+              <>
+                <p>
+                  You successfully signed in with Passage.
+                  <br />
+                  Your username is: <b>{props.username}</b>
+                </p>
+              </>
+            ) : (
+              <>
+                You have not logged in and cannot view the dashboard.
+                <br /> <br />
+                <Link
+                  href="/"
+                  className="underline font-bold hover:text-blue-600 "
+                >
+                  Login to continue.
+                </Link>
+              </>
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 };
 
--- components/banner.tsx
+++ components/banner.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import styles from "../styles/Banner.module.css";
 
 export default function Banner() {
   return (
