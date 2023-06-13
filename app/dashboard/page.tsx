ad048d969b2967d5690e17a6d1f656ce764d2b22
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-06-13 12:08:12 +0530

    added getCurrentUser hook

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,86 +1,52 @@
-'use client'
-
-import Passage from "@passageidentity/passage-node";
-import { GetServerSideProps, GetServerSidePropsContext, NextPage } from "next";
-import Link from "next/link"
-import { ReactNode, useEffect } from "react";
-import styles from '../../styles/App.module.css'
-
-
+import { NextPage } from "next";
+import Link from "next/link";
+import styles from "../../styles/App.module.css";
+import { getCurrentUser } from "@/hooks/getCurrentUser";
 
 interface DashboardProps {
-  isAuthorized: boolean;
-  username: string;
   appID: string;
-};
+}
 
-const Dashboard: NextPage<DashboardProps> = ({ isAuthorized, username, appID }) => {
+const Dashboard: NextPage<DashboardProps> = async ({ appID }) => {
+  const { props } = await getCurrentUser();
+  console.log(props);
 
-  useEffect(() => {
-    require('@passageidentity/passage-elements/passage-profile');
-  }, []);
-
-  const authorizedBody =
+  const authorizedBody = (
     <>
       <p>You successfully signed in with Passage.</p>
 
-      <p>Your username is: <b>{username}</b></p>
+      <p>
+        Your username is: <b>{props.username}</b>
+      </p>
 
-      <p><passage-profile app-id={appID}></passage-profile></p>
-
+      <p>
+        <passage-profile app-id={appID}></passage-profile>
+      </p>
     </>
+  );
 
-  const unauthorizedBody =
+  const unauthorizedBody = (
     <>
       You have not logged in and cannot view the dashboard.
       <br /> <br />
-
-      <Link href="/" className={styles.link}>Login to continue.</Link>
+      <Link href="/" className={styles.link}>
+        Login to continue.
+      </Link>
     </>
-
+  );
 
   return (
     <div className={styles.mainContainer}>
       <div className={styles.dashboard}>
-        <div className={styles.title}>{isAuthorized ? 'Welcome!' : 'Unauthorized'}</div>
-        <div className={styles.message}>
-          {isAuthorized ? authorizedBody : unauthorizedBody}
+        <div className={styles.title}>
+          {props.isAuthorized ? "Welcome!" : "Unauthorized"}
         </div>
-
+        <div className={styles.message}>
+          {props.isAuthorized ? authorizedBody : unauthorizedBody}
+        </div>
       </div>
     </div>
+  );
+};
 
-
-  )
-}
-
-export const getServerSideProps: GetServerSideProps<DashboardProps> = async (context: GetServerSidePropsContext) => {
-  // getServerSideProps runs server-side only and will never execute on the client browser
-  // this allows the safe use of a private Passage API Key
-  const passage = new Passage({
-    appID: process.env.PASSAGE_APP_ID!,
-    apiKey: process.env.PASSAGE_API_KEY!,
-    authStrategy: "HEADER",
-  });
-  try {
-    const authToken = context.req.cookies['psg_auth_token'];
-    const req = {
-      headers: {
-        authorization: `Bearer ${authToken}`,
-      },
-    };
-    const userID = await passage.authenticateRequest(req);
-    if (userID) {
-      // user is authenticated
-      const { email, phone } = await passage.user.get(userID);
-      const identifier = email ? email : phone;
-      return { props: { isAuthorized: true, username: identifier, appID: passage.appID } };
-    }
-  } catch (error) {
-    // authentication failed
-  }
-  return { props: { isAuthorized: false, username: '', appID: passage.appID } };
-}
-
-
-export default Dashboard
+export default Dashboard;
